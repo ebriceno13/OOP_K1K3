72661aba1ce180856d7c21eb0157392e5b3865c2
ebriceno13 <enrique.briceno.martinez@ucreativa.com>
2021-06-02 01:05:25 -0600

    Trabajo en clase #3

--- src/com/ucreativa/Main.java
+++ src/com/ucreativa/Main.java
@@ -1,13 +1,18 @@
 package com.ucreativa;
 
 import com.ucreativa.familia.Guiss;
-import com.ucreativa.familia.Kike;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Kike yo = new Kike();
         Guiss mama = new Guiss("Diay Pa");
+
+        mama.setAge();
+
+        int edad = mama.getAge();
+
+        System.out.println("La edad de mi mama Guiss es: " + edad);
+        System.out.println("El hobby de mi mama es: " + mama.getHobby());
     }
 }
--- src/com/ucreativa/familia/Guiss.java
+++ src/com/ucreativa/familia/Guiss.java
@@ -2,7 +2,24 @@ package com.ucreativa.familia;
 
 public class Guiss {
 
+    private int age;
+    private String hobby;
+
     public Guiss(String saludo) {
+        this.age = 69;
+        this.hobby = "Jardineria";
         System.out.println(saludo);
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setAge() {
+        this.age = this.age + 1;
+    }
+
+    public int getAge() {
+        return this.age;
     }
 }

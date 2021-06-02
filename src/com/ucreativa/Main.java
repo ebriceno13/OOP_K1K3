36f01388bcbbf63f1d55aa52de1ca4c41feab7c6
ebriceno13 <enrique.briceno.martinez@ucreativa.com>
2021-06-01 23:47:13 -0600

    Trabajo en clase #1

---
+++ src/com/ucreativa/Main.java
@@ -0,0 +1,12 @@
+package com.ucreativa;
+
+import com.ucreativa.familia.Kike;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Kike ricolino = new Kike();
+
+    }
+}
---
+++ src/com/ucreativa/familia/Kike.java
@@ -0,0 +1,8 @@
+package com.ucreativa.familia;
+
+public class Kike {
+
+    public Kike() {
+        System.out.println("Yo soy Kike");
+    }
+}
